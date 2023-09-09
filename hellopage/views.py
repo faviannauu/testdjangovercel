239a4b242cb09e01f2e1a663518dab327af67cd1
faviann <favian.naufal@ui.ac.id>
2023-09-09 20:06:12 +0700

    modifying template

--- hellopage/views.py
+++ hellopage/views.py
@@ -4,8 +4,5 @@ from .models import Task
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
-    context = {
-        "task" : tasks,
-    }
-    return render(request, "index.html", context)
+
+    return render(request, "index.html")
