2facf73dfa443315753ff4eb072ce40ce70eb842
Sylphiann <Hydrolilium@gmail.com>
2024-09-18 09:13:46 +0700

    Init

--- testdjangovercel/wsgi.py
+++ testdjangovercel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DeployTask project.
+WSGI config for testdjangovercel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DeployTask.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testdjangovercel.settings')
 
 application = get_wsgi_application()
 
